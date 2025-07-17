70a03052973179d71abafa21b586502b85f00ca0
Johanna_VJ <165029467+Chaves-Johanna-Milagro@users.noreply.github.com>
2025-07-16 23:12:29 -0300

    UI plantilla pt1
    Se creo una ui por defecto para ahorrar al momento de reutilizarla

--- VamosALaEscuela/Assets/Scripts/Player/PlayerMove.cs
+++ VamosALaEscuela/Assets/Scripts/Player/PlayerMove.cs
@@ -18,14 +18,15 @@ public class PlayerMove : MonoBehaviour
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved)
+            if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved &&
+            !ClickInUIStatus.IsPointerOverUI_Mobile())
             {
                 SetTargetPos(touch.position);
             }
         }
 
         //  Click en PC para testear en el editor
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !ClickInUIStatus.IsPointerOverUI_PC())
         {
             SetTargetPos(Input.mousePosition);
         }
@@ -36,9 +37,7 @@ public class PlayerMove : MonoBehaviour
             transform.position = Vector3.MoveTowards(transform.position, _targetPos, speed * Time.deltaTime);
 
             // Parar si llegó o esta en la ui
-            if (Vector3.Distance(transform.position, _targetPos) < 0.05f || 
-                ClickInUIStatus.IsPointerOverUI_PC() ||
-                ClickInUIStatus.IsPointerOverUI_Mobile()) _isMoving = false;
+            if (Vector3.Distance(transform.position, _targetPos) < 0.05f) _isMoving = false;
          
         }
     }
