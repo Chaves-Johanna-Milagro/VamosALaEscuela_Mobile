74fe638e4e10cfe8aa16eddf6d70e6a4fef70f13
Johanna_VJ <165029467+Chaves-Johanna-Milagro@users.noreply.github.com>
2025-07-21 01:47:50 -0300

    Nivel1 pt5
    Animacion  del player con pijama

---
+++ VamosALaEscuela/Assets/Scripts/Player/PlayerAnim1.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class PlayerAnim1 : MonoBehaviour
+{
+    private PlayerMove _pMove;
+
+    private Animator _anim;
+
+    private void Start()
+    {
+        _pMove = GetComponent<PlayerMove>();
+
+        _anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (_pMove.IsMoving())
+        {
+            Vector3 targetPos = _pMove.NextPos();
+            WalkAnim(targetPos);
+        }
+
+    }
+
+    private void LateUpdate()
+    {
+        IdleAnim();
+    }
+
+    private void WalkAnim(Vector3 newPos)
+    {
+        ResetAnim();
+
+        if (newPos.x > transform.position.x)
+        {
+            _anim.SetBool("R_Walk_PJ", true);
+        }
+        else if (newPos.x < transform.position.x)
+        {
+            _anim.SetBool("L_Walk_PJ", true);
+        }
+    }
+    private void IdleAnim()
+    {
+        ResetAnim();
+
+        if (!_pMove.IsMoving()) _anim.SetBool("Idle_PJ", true);
+    }
+
+    private void ResetAnim()
+    {
+        _anim.SetBool("Idle_PJ", false);
+        _anim.SetBool("R_Walk_PJ", false);
+        _anim.SetBool("L_Walk_PJ", false);
+    }
+}
